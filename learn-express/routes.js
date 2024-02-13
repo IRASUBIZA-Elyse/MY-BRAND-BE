518aa118d7ef18b755d8a8c1b00906d6a74c4cb3
IRASUBIZA-Elyse <h.irasubizaelyse@gmail.com>
2024-02-13 02:36:01 +0200

    setup routs schema and connections nedded

---
+++ learn-express/routes.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const Post = require("./models/Post");
+const router = express.Router();
+
+router.get("/posts", async (req, res) => {
+  const posts = await Post.find();
+  res.send(posts);
+});
+module.exports = router;
+
+router.post("/posts", async (req, res) => {
+  const post = new Post({
+    title: req.body.title,
+    content: req.body.content,
+  });
+  await post.save();
+  res.send(post);
+});
+router.get("/posts/:id", async (req, res) => {
+  const post = await Post.findOne({ _id: req.params.id });
+  res.send(post);
+});
