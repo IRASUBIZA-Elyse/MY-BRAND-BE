16292cc14c1a1f1b6f61f0f9141949bbdc43b5a0
IRASUBIZA-Elyse <h.irasubizaelyse@gmail.com>
2024-02-22 15:50:59 +0200

    added the middleware

--- learn-express ts/src/models/User.ts
+++ learn-express ts/src/models/User.ts
@@ -1,36 +1,64 @@
-import mongoose, { Schema, model, Document } from "mongoose";
+// import mongoose, { Schema, model, Document } from "mongoose";
+// import bcrypt from "bcrypt";
+// // import passportLocalMongoose from "passport-local-mongoose";
+// export interface IUserDocument extends Document {
+//   userName: String;
+//   email: string;
+//   password: string;
+//   isValidPassword(password: string): Promise<boolean>;
+// }
+// const UserSchema = new Schema<IUserDocument>({
+//   userName: { type: "string", required: true },
+//   email: { type: String, require: true, unique: true },
+//   password: {
+//     type: String,
+//     required: true,
+//   },
+// });
+// UserSchema.pre("save", async function (next) {
+//   const user = this;
+//   const hash = await bcrypt.hash(user.password, 10);
+
+//   this.password = hash;
+//   next();
+// });
+
+// UserSchema.methods.isValidPassword = async function (
+//   password: string
+// ): Promise<boolean> {
+//   const user = this;
+//   const compare = await bcrypt.compare(password, user.password);
+
+//   return compare;
+// };
+
+// // UserSchema.plugin(passportLocalMongoose);
+// const User = model("user", UserSchema);
+
+// export default User;
+import mongoose, { Schema, Document } from "mongoose";
 import bcrypt from "bcrypt";
-// import passportLocalMongoose from "passport-local-mongoose";
-export interface IUserDocument extends Document {
+
+export interface IUser extends Document {
+  username: string;
   email: string;
   password: string;
-  isValidPassword(password: string): Promise<boolean>;
 }
-const UserSchema = new Schema<IUserDocument>({
-  email: { type: String, require: true, unique: true },
-  password: {
-    type: String,
-    required: true,
-  },
-});
-UserSchema.pre("save", async function (next) {
-  const user = this;
-  const hash = await bcrypt.hash(user.password, 10);
 
-  this.password = hash;
+const userSchema = new Schema({
+  username: { type: String, required: true },
+  password: { type: String, required: true },
+  email: { type: String, required: true },
+});
+
+userSchema.pre("save", async function (next) {
+  const user = this as IUser;
+  if (!user.isModified("password")) return next();
+
+  const salt = await bcrypt.genSalt(10);
+  const hashedPassword = await bcrypt.hash(user.password, salt);
+  user.password = hashedPassword;
   next();
 });
 
-UserSchema.methods.isValidPassword = async function (
-  password: string
-): Promise<boolean> {
-  const user = this;
-  const compare = await bcrypt.compare(password, user.password);
-
-  return compare;
-};
-
-// UserSchema.plugin(passportLocalMongoose);
-const User = model("user", UserSchema);
-
-export default User;
+export default mongoose.model<IUser>("User", userSchema);
