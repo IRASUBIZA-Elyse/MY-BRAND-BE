2270d29198871a309620ccb01deec774751832b8
IRASUBIZA-Elyse <h.irasubizaelyse@gmail.com>
2024-02-27 18:50:36 +0200

    changed directories

--- learn-express-ts/dist/tools/config.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const config = {
-    jwtSecret: "JWT Secret",
-    jwtSession: { session: false },
-};
-exports.default = config;
