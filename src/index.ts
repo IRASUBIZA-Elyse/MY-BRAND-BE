b1f0e048bd7584c79d4fd7f62ac598be961ea2cb
IRASUBIZA-Elyse <h.irasubizaelyse@gmail.com>
2024-03-01 14:11:53 +0200

    added coverage

--- src/index.ts
+++ src/index.ts
@@ -8,28 +8,7 @@ import swaggerJSDoc from "swagger-jsdoc";
 import swaggerUi from "swagger-ui-express";
 import * as swaggerDocument from "./swagger.json";
 import cors from "cors";
-
-dotenv.config();
 const app = express();
-// const options = {
-//   definition: {
-//     openapi: "3.0.0",
-//     info: {
-//       title: "Blog API",
-//       version: "1.0.0",
-//       description: "This is the API for the blog",
-//     },
-//     servers: [
-//       {
-//         url: "http://localhost:4000",
-//       },
-//     ],
-//   },
-//   apis: ["./src/routes/routes.ts"],
-// };
-// const swaggerSpec = swaggerJSDoc(options);
-
-const mongoUrl: string = process.env.MONGODB_URL!;
 
 app.use(express.json());
 app.use("/api", routes);
