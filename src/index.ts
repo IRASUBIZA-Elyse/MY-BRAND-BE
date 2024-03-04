88bf0cc372f03ea959f6691dc7808dd298024af1
IRASUBIZA-Elyse <h.irasubizaelyse@gmail.com>
2024-03-04 13:15:15 +0200

    changed cors line on index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,6 @@
 import express from "express";
 import mongoose from "mongoose";
 import routes from "./routes/routes";
-//import userRouter from "./routes/userRoutes";
 import dotenv from "dotenv";
 
 import swaggerJSDoc from "swagger-jsdoc";
@@ -14,6 +13,5 @@ app.use(express.json());
 app.use(cors());
 app.use("/api", routes);
 app.use("/swagger", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
-//app.use("/api", userRouter);
 
 export default app;
